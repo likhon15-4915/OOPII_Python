9fb6a73e8438706579aae4f1fb09dbc9f40b59d2
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-02-07 20:06:26 +0600

    Update 2. loop(while).py

--- 2. loop(while).py
+++ 2. loop(while).py
@@ -2,5 +2,6 @@ i=1
 while(i<10):
     i = i + 1
     if(i==6):
-        continue
+        continue        #6 will be skipped
+                        # for break- break from the condition
     print(i)
