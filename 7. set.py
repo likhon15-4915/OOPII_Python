807c33418947a2f38abe9a79d1590d857859ba4c
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-03-11 16:47:05 +0600

    Create 7. set.py

---
+++ 7. set.py
@@ -0,0 +1,20 @@
+"""built in function in Set
+all()
+any()
+len()
+max()
+min()
+sorted()
+sum()
+enumerate()"""
+
+numbers={3,1,5,9}
+numbers.add(7)
+print(numbers)
+
+print(len(numbers))
+print(max(numbers))
+print(min(numbers))
+print(sum(numbers))
+print(sorted(numbers))
+
