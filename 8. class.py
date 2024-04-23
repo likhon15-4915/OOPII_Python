c6f4e9c360c5b3416c0fc1af008c2b73d66e38de
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-04-23 09:55:19 +0600

    Create 8. class.py

---
+++ 8. class.py
@@ -0,0 +1,10 @@
+class Employee:
+    E_name="XYZ"
+    E_age=30
+    E_id=710003053
+    def __init__(Self):
+        print(Self.E_name,Self.E_age,Self.E_id)
+    def Working(Self):
+        print("Employee Working as a teacher")
+employee1=Employee()
+employee1.Working()
